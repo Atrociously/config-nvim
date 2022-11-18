9f2ed419d99a3bc89a0b866a8770c1bd23b64e83
Jack Morrison <jackamorr@gmail.com>
2022-11-17 21:05:00 -0600

    Changed buffer movement so <esc> no longer does it

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -3,8 +3,9 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 keymap("v", "<C-c>", "\"+y", opts)
-keymap("n", "<C-[>", "<cmd>bprevious<cr>", opts)
-keymap("n", "<C-]>", "<cmd>bnext<cr>", opts)
+keymap("n", "<leader>b[", "<cmd>bprevious<cr>", opts)
+keymap("n", "<leader>b]", "<cmd>bnext<cr>", opts)
+keymap("n", "<esc>", "", opts)
 
 keymap("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", opts)
 keymap("n", "<leader>xx", "<cmd>TroubleToggle<cr>", opts)
